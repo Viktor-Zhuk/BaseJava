a2380c08a4a0d6a669f6ed35fd1dd4e3f8fd5a96
Viktor <v.zhuk@mahorka.com>
2022-02-13 14:16:56 +0300

    Test for BaseJava. Done by Viktor Zhuk.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,70 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int numberResume = 0;
 
-    void clear() {
+
+    public void clear() {
+        for (int i = 0; i < numberResume; i++) {
+            storage[i] = null;
+        }
+        numberResume = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume resume) {
+        boolean isNewResume = true;
+        for (int i = 0; i < numberResume; i++) {
+            if (storage[i].uuid == resume.uuid) {
+                isNewResume = false;
+                break;
+            }
+        }
+        if (isNewResume) {
+        storage[numberResume] = resume;
+        numberResume++;
+        }
     }
 
-    Resume get(String uuid) {
-        return null;
+    public Resume get(String uuid) {
+        Resume rezume = new Resume();
+        for (int i = 0; i < numberResume; i++) {
+            if (storage[i].uuid == uuid) {
+                rezume = storage[i];
+                break;
+            }
+        }
+        if (rezume.uuid == null) {
+            System.out.println("Resume not found");
+        }
+        return rezume;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
+        boolean isResume = false;
+        for (int i = 0; i < numberResume; i++) {
+            if (storage[i].uuid == uuid) {
+                isResume = true;
+            }
+            if (isResume) {
+                storage[i] = storage[i + 1];
+            }
+        }
+        numberResume--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        return new Resume[0];
+    public Resume[] getAll() {
+        Resume[] filledResume = new Resume[numberResume];
+        for (int i = 0; i < numberResume; i++) {
+            filledResume[i] = storage[i];
+        }
+        return filledResume;
     }
 
-    int size() {
-        return 0;
+    public int size() {
+        return numberResume;
     }
 }
